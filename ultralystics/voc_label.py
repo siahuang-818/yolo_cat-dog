9e4ecbb0e55c68c46faec9842604a1640b694cbe
siahuang-818 <h.siyue818@gmail.com>
2025-03-24 10:50:31 +0800

    3 rd version

---
+++ ultralystics/voc_label.py
@@ -0,0 +1,66 @@
+import xml.etree.ElementTree as ET
+import os
+from os import getcwd
+
+sets = ['train', 'val', 'test']
+classes = ['cat', 'dog']
+abs_path = os.getcwd()
+print(abs_path)
+
+def convert(size, box):
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[1]) / 2.0 - 1
+    y = (box[2] + box[3]) / 2.0 - 1
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return x, y, w, h
+
+def convert_annotation(image_id):
+    in_file = open('Annotations/%s.xml' % (image_id), encoding='UTF-8')
+    out_file = open('labels/%s.txt' % (image_id), 'w')
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult) == 1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
+             float(xmlbox.find('ymax').text))
+        b1, b2, b3, b4 = b
+        if b2 > w:
+            b2 = w
+        if b4 > h:
+            b4 = h
+        b = (b1, b2, b3, b4)
+        bb = convert((w, h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+wd = getcwd()
+for image_set in sets:
+    if not os.path.exists('labels/'):
+        os.makedirs('labels/')
+    image_ids = open('ImageSets/%s.txt' % (image_set)).read().strip().split()
+    list_file = open('%s.txt' % (image_set), 'w')
+    for image_id in image_ids:
+        # 检查图片的实际文件类型
+        if os.path.exists('images/%s.jpg' % image_id):
+            image_path = 'images/%s.jpg' % image_id
+        elif os.path.exists('images/%s.png' % image_id):
+            image_path = 'images/%s.png' % image_id
+        else:
+            continue  # 如果没有找到图片，跳过该 image_id
+
+        list_file.write(abs_path + '/' + image_path + '\n')
+        convert_annotation(image_id)
+    list_file.close()
